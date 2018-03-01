a0ec352caea30a8b1e792a15be8cb68c11014f1c
thomasvdj <thomasvanderjagt@gmail.com>
2018-03-01 19:28:46 +0100

    Dit is mijn bijdrage.

---
+++ File.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def read_file():
+    all_data = np.loadtxt("a_example.in", dtype=int, delimiter = " ", skiprows = 0)
+    first_row = all_data[0,:]
+    all_data = all_data[1:all_data.shape[0],:]
+    return first_row, all_data
+
+print(all_data)
+print("")
+print(first_row)
