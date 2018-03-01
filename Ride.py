9b77010c052a42622f2ee8aed7c6d9352e6c5bc6
Gregory Kevin Hill <gkh998@gmail.com>
2018-03-01 19:08:15 +0100

    Add initial Point & Ride declarations

---
+++ Ride.py
@@ -0,0 +1,15 @@
+
+import Point
+
+
+class Ride:
+    """Ride"""
+
+    sp = Point(0,0)
+    ep = Point(0,0)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3.6
 
 import numpy as np
-import types
+import Point
+import
 
 """
 Main project for hashcode
 """
 
 
+
+
 def main():
-    print("Hello, World! sdfasdklflaksjfklsdj ", 5)
+    # Read out file
 
-    a = []
 
-    for i in range(1,5):
-        print(i)
-        a.append(i)
-    
+
 
 if __name__ == "__main__":
     main()
