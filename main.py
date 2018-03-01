d3decc868d27f4613f355507a572a91e332a33be
Gregory Kevin Hill <gkh998@gmail.com>
2018-03-01 18:23:33 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.6
+
+import numpy as np
+import types
+
+"""
+Main project for hashcode
+"""
+
+
+def main():
+    print("Hello, World! sdfasdklflaksjfklsdj ", 5)
+
+    a = []
+
+    for i in range(1,5):
+        print(i)
+        a.append(i)
+    
+
+if __name__ == "__main__":
+    main()
